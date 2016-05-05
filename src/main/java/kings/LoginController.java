9cc039958b8850c228459930b92e6c712367f526
ikoishman <ikoishman@ebay.com>
2016-05-05 17:02:46 +0300

    get request works

--- src/main/java/kings/LoginController.java
+++ src/main/java/kings/LoginController.java
@@ -1,19 +1,18 @@
 package kings;
 
-import java.util.Map;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
 
-	@RequestMapping(value = "/login", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
-	protected @ResponseBody Player login(@RequestBody Map<String, String> paramsMap) {
-		return new Player(paramsMap.get("userName"), paramsMap.get("password"));
+	@RequestMapping(value = "/login", method = RequestMethod.GET) 
+	protected @ResponseBody Player login(@RequestParam(required = false, value = "userName") String userName,
+			@RequestParam(required = false, value = "password") String password) {
+		return new Player(userName, password);
 	}
   
 	
