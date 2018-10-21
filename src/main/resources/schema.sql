3745bb26155128c5b671edb95c50fd1ca2104a24
igorkoishman <igorkoishman@gmail.com>
2018-10-22 00:33:42 +0300

    chnage prop

--- src/main/resources/schema.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS `bn_categories`;
-
-CREATE TABLE `voter` (
-  `id` int(20) unsigned NOT NULL,
-  `post` bigint(20) unsigned NOT NULL,
-  `vote` int (20) unsigned DEFAULT NULL,
-  PRIMARY KEY (`post`,`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
